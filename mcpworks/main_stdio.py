62532475ad2adac868ed70d6443175d1406a51d0
callbinuvarghese <call.binu.varghese@gmail.com>
2025-05-29 18:10:36 -0400

    initial commit

---
+++ mcpworks/main_stdio.py
@@ -0,0 +1,18 @@
+
+# main.py
+from server import mcp
+# Import tools so they get registered via decorators
+import tools.csv_tools
+import tools.parquet_tools
+import tools.research_tools
+
+def main():
+    print("Hello from mcpworks!")
+
+# Entry point to run the server
+if __name__ == "__main__":
+    #mcp.run()
+    mcp.run(transport='stdio')
+
+# if __name__ == "__main__":
+#     main()
